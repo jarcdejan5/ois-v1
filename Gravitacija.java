1c17dfc6fedb2be004ab92c146fccfb9d524c634
Jure Zupancic <jure.zupancic12@gmail.com>
2022-02-22 19:14:17 +0100

    dodajanje izracuna

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,21 @@
+
 public class Gravitacija{
 
 	public static void main(String[] args){
 		System.out.println("OIS je zakon!");
+	}
+	
+	public static double izracun(double visina){
+		/*BigDecimal C = new BigDecimal(6.674 * Math.pow(10,-11));
+		BigDecimal M = new BigDecimal(5.972 * Math.pow(10,24));
+		BigDecimal r = new BigDecimal(6.371 * Math.pow(10,6));
+		
+		return new BigDecimal(C.multiply(M).divide((r.add(visina)).pow(2)));*/
+		
+		double C = 6.674 * Math.pow(10,-11);
+		double M = 5.972 * Math.pow(10,24);
+		double r = 6.371 * Math.pow(10,6);
+		return (C*M)/Math.pow(r+visina,2);
 	}
 
 }
