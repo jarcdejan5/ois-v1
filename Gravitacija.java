bbeb419d6f62d9946a1db06ad1d5cf713aa28fd0
theonlyrealmax <max.crypto@hotmail.com>
2022-02-22 19:21:18 +0100

    Dodajanje izpisa

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
 public class Gravitacija{
 
 	public static void main(String[] args){
 		System.out.println("OIS je zakon!");
 	}
 
+	public static void izpis(double visina, double gravPosp) {
+		System.out.println("Pri višini " + visina + " je gravitacijski pospešek " + gravPosp);
+	}
 }
